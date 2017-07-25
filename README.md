81fe5f30c96939c06df6ecee4db8bebd0c5131fc
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-25 11:13:23 -0700

    Use JavaScript Standard Style

--- README.md
+++ README.md
@@ -5,11 +5,18 @@ var assert = require('assert')
 assert.deepEqual(
   unmarked({
     content: [
-      { definition: 'Agreement' },
-      'Agreement is an agreement' ] }),
-  [ { level: 'info',
+      {definition: 'Agreement'},
+      'Agreement is an agreement'
+    ]
+  }),
+  [
+    {
+      level: 'info',
       message: '"Agreement" is an unmarked defined-term use.',
       path: ['content', 1],
       source: 'commonform-unmarked-uses',
-      url: null } ])
+      url: null
+    }
+  ]
+)
 ```
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-module.exports = function unmarkedUses(form) {
+var analyze = require('commonform-analyze')
+var annotator = require('commonform-regexp-annotator')
+
+module.exports = function unmarkedUses (form) {
   // Analyze the form, then pull out metadata about the terms defined within.
   var definitions = analyze(form).definitions
   // Create a phrase annotator matching strings equal to the terms defined.
   return annotator(
-    Object.keys(definitions)
-      .map(function(term) {
-        return new RegExp('(\\b' + term + '\\b)', 'g') }),
-    function(form, path, re, match) {
+    Object.keys(definitions).map(function (term) {
+      return new RegExp('(\\b' + term + '\\b)', 'g')
+    }),
+    function (form, path, re, match) {
       var string = match[1]
       return {
         level: 'info',
         message: ( '"' + string + '" is an unmarked defined-term use.' ),
         path: path,
         source: 'commonform-unmarked-uses',
-        url: null } })
-      // Apply the annotator to the form.
-      (form) }
-
-var analyze = require('commonform-analyze')
-var annotator = require('commonform-regexp-annotator')
+        url: null
+      }
+    })
+    // Apply the annotator to the form.
+    (form)
+}
